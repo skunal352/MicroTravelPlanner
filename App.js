b46d08436597ec219a4308fb699bf9e54cd53172
skunal352 <skunal352@gmail.com>
2026-03-31 03:18:52 -0700

    Set up navigation with stack screens in App.js

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import HomeScreen from './screens/HomeScreen';
+import CreateTripScreen from './screens/CreateTripScreen';
+import SavedTripsScreen from './screens/SavedTripsScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="CreateTrip" component={CreateTripScreen} />
+        <Stack.Screen name="SavedTrips" component={SavedTripsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
